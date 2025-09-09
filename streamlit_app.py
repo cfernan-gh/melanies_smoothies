05229b289f29b9628e6f9e934e49c28bb8b74a85
cfernan-gh <ccfernan.eco@gmail.com>
2025-09-09 10:40:52 +0200

    Update streamlit_app.py
    space edit again

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,11 +40,12 @@ ingredients_list = st.multiselect(
 
 #use 4 spaces for identation instead of tabs
 if ingredients_list :
-       ingredients_string=''
-         for fruit_chosen in ingredients_list:
-            ingredients_string +=fruit_chosen+' '
-            smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-            sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+    ingredients_string=''
+  
+    for fruit_chosen in ingredients_list:
+      ingredients_string +=fruit_chosen+' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     #st.write(ingredients_string)
 
